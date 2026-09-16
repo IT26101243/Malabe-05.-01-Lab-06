9f67dae135ae3cffdc7307c60578f6dea7a3911a
IT26101243 <it26101243@my.sliit.lk>
2026-09-16 06:09:57 +0530

    Update IT26101243Lab6Q1.java

--- Q1/IT26101243Lab6Q1.java
+++ Q1/IT26101243Lab6Q1.java
@@ -4,15 +4,13 @@ public class IT26101243Lab6Q1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt the user for a number
+        
         System.out.print("Enter a number: ");
         double num = scanner.nextDouble();
 
-        // Calculate square and square root
-        double square = num * num; // or Math.pow(num, 2)
+        double square = num * num; 
         double squareRoot = Math.sqrt(num);
 
-        // Print outputs matching expected format
         System.out.println("The square of " + num + " is: " + square);
         System.out.println("The square root of " + num + " is: " + squareRoot);
 
