375253d020c6c6642f22d66d366155f5533fae68
IT26101243 <it26101243@my.sliit.lk>
2026-09-16 06:13:19 +0530

    Update IT26101243Lab6Q3.java

--- Q3/IT26101243Lab6Q3.java
+++ Q3/IT26101243Lab6Q3.java
@@ -12,10 +12,8 @@ public class IT26101243Lab6Q3 {
         System.out.print("Enter a number: ");
         int num = scanner.nextInt();
 
-        // Loop runs until the user enters -99
         while (num != -99) {
             
-            // Check if input is positive
             if (num > 0) {
                 sumOfSquares = sumOfSquares + (num * num);
                 count++;
@@ -23,12 +21,10 @@ public class IT26101243Lab6Q3 {
                 System.out.println("Invalid input. Please enter a positive integer or -99 to terminate");
             }
 
-            // Ask for the next number
             System.out.print("Enter a number: ");
             num = scanner.nextInt();
         }
 
-        // Calculate Root Mean Square
         if (count > 0) {
             double rms = Math.sqrt(sumOfSquares / count);
             System.out.println("\nThe Root Mean Square (RMS) is: " + rms);
